cd452d81842d8f7f710c36cb90a252b67245e444
Harmy <adrian.lissot@gmail.com>
2019-03-12 08:10:32 +0100

    progress

--- Hop/Hop.App/Program.fs
+++ Hop/Hop.App/Program.fs
@@ -2,6 +2,9 @@
 open System
 open System.Windows
 open System.Windows.Controls
+open System.Windows.Media
+open System.Windows.Media.Imaging
+open System.Windows.Input
 
 type Model =
     {
@@ -24,11 +27,71 @@ let init () =
     let results = autocomplete arguments hop
     { Arguments = arguments; Items = results; Message = String.Empty; Hop = hop }
 
+let createCommand execute =
+    {
+        new ICommand with
+            member this.Execute (argument) = execute argument
+            member this.CanExecute (_) = true
+            member this.add_CanExecuteChanged(_) = ()
+            member this.remove_CanExecuteChanged(_) = ()
+    }
+
 let view model =
+    let argumentsGrid = new Grid ()
+    argumentsGrid.ColumnDefinitions.Add (ColumnDefinition( Width = GridLength.Auto ))
+    argumentsGrid.ColumnDefinitions.Add (ColumnDefinition())
+    let argumentsListView = new ListView ()
+    for item in model.Arguments.Tail do
+        let nameTextBlock = new TextBlock ( Text = item.Name )
+        //let image = new Image ( Source = new BitmapImage (new Uri(item.Image)) )
+        let itemGrid = new Grid ()
+        itemGrid.ColumnDefinitions.Add (ColumnDefinition())
+        itemGrid.ColumnDefinitions.Add (ColumnDefinition())
+        itemGrid.Children.Add nameTextBlock |> ignore
+        //itemGrid.Children.Add image |> ignore
+        Grid.SetColumn (nameTextBlock, 0)
+        //Grid.SetColumn (image, 1)
+        let listViewItem = new ListViewItem ( Content = itemGrid )
+        argumentsListView.Items.Add listViewItem |> ignore
+    let queryTextBox = new TextBox ( Text = model.Arguments.Head )
+    Grid.SetColumn (argumentsListView, 0)
+    Grid.SetColumn (queryTextBox, 1)
+    argumentsGrid.Children.Add argumentsListView |> ignore
+    argumentsGrid.Children.Add queryTextBox |> ignore
+
+    let itemListView = new ListView ()
+    itemListView.InputBindings.Add (new InputBinding(createCommand (fun o -> )))
+    for item in model.Items do
+        let itemGrid = new Grid ()
+        itemGrid.ColumnDefinitions.Add (ColumnDefinition())
+        itemGrid.ColumnDefinitions.Add (ColumnDefinition())
+        let identifierGrid = new Grid ()
+        identifierGrid.RowDefinitions.Add (RowDefinition())
+        identifierGrid.RowDefinitions.Add (RowDefinition())
+        let nameTextBlock = new TextBlock ( Text = item.Name )
+        let descriptionTextBlock = new TextBlock ( Text = item.Description )
+        //let image = new Image ( Source = new BitmapImage (new Uri(item.Image)) )
+        Grid.SetRow (nameTextBlock, 0)
+        Grid.SetRow (descriptionTextBlock, 1)
+        identifierGrid.Children.Add nameTextBlock |> ignore
+        identifierGrid.Children.Add descriptionTextBlock |> ignore
+        Grid.SetColumn (identifierGrid, 0)
+        //Grid.SetColumn (image, 1)
+        itemGrid.Children.Add identifierGrid |> ignore
+        //itemGrid.Children.Add image |> ignore
+        let listViewItem = new ListViewItem ( Content = itemGrid )
+        itemListView.Items.Add listViewItem |> ignore
+
     let mainGrid = new Grid ()
-    mainGrid.ColumnDefinitions.Add (ColumnDefinition())
-    mainGrid.ColumnDefinitions.Add (ColumnDefinition())
-    mainGrid
+    mainGrid.RowDefinitions.Add (RowDefinition( Height = GridLength.Auto ))
+    mainGrid.RowDefinitions.Add (RowDefinition())
+    Grid.SetRow (argumentsGrid, 0)
+    Grid.SetRow (itemListView, 1)
+    mainGrid.Children.Add argumentsGrid |> ignore
+    mainGrid.Children.Add itemListView |> ignore
+    let window = new Window ()
+    window.Content <- mainGrid
+    window
 
 let update model message =
     match message with
@@ -52,5 +115,6 @@ let update model message =
 [<STAThread>]
 let main argv = 
     let app = new Application ()
-    let mainWindow = new Window ()
+    let model = init ()
+    let mainWindow = view model
     app.Run mainWindow
